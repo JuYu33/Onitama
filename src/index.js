fe27ebd7831966f9252db9a533146d300c8cd6e3
cupnoodl3s <cup.noodl3s@gmail.com>
2017-07-07 18:02:42 -0700

    delete test

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
@@ -9,7 +9,7 @@ const Square = (props) => (
   </button>
 )
 
-class Board extends React.Component {
+class Board extends Component {
   constructor() {
     super();
     this.state = {
@@ -116,7 +116,7 @@ class Board extends React.Component {
   }
 }
 
-class Game extends React.Component {
+class Game extends Component {
   constructor() {
     super();
     this.state = {
@@ -132,7 +132,7 @@ class Game extends React.Component {
           <Board />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div>{/* status */}This is Onitama</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
@@ -147,23 +147,7 @@ ReactDOM.render(
   document.getElementById('container')
 );
 
-function calculateWinner(squares) {
-  /*const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  */
+function calculateWinner(condition) {
+
   return null;
 }
