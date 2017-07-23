eaebbde0ea38ba5aad6544cdd39f894cc91dc4d3
cupnoodl3s <cup.noodl3s@gmail.com>
2017-07-23 01:59:17 -0700

    Card selection and movement updated

--- src/index.js
+++ src/index.js
@@ -16,31 +16,11 @@ const Start = (props) => (
   </button>
 )
 
-class Cards extends Component {
-  constructor() {
-    super();
-    this.state = {
-    }
-  }
-
-  //TODO: add card images
-  render() {
-    return (
-      <div>
-        <div id="player1-box">
-          <div className="player-card selected-card" onClick={()=>this.handleClick}>
-            <h1>{this.props.nameOfCard1}</h1>
-
-          </div>
-          <div className="player-card">
-            <h1>{this.props.nameOfCard2}</h1>
-
-          </div>
-        </div>
-      </div>
-    )
-  }
-}
+const Card = (props) => (
+  <div className={props.className} onClick={() => props.onClick()}>
+    <h1>{props.card}</h1>
+  </div>
+)
 
 class Board extends Component {
   constructor() {
@@ -53,43 +33,66 @@ class Board extends Component {
                 ['o','o','O','o','o']],
       //TODO: [x,y] remember to invert y
       //p2: invert x not y
+      isCaptured: {O: false, X: false},
       cards:  {
-                triangle: [[0,1],[1,-1],[-1,-1]],
-                invTri: [[0,-1],[-1,1],[1,1]],
-                upDown: [[0,-1],[0,1]],
-                backDiag: [[1,1],[-1,-1]],
-                forwDiag: [[-1,1],[1,-1]],
-                cross: [[-1,0],[0,1],[1,0],[0,-1]],
-                front: [[-1,1],[0,1],[1,1]]
+                Tiger: [[0,2], [0,-1]],
+                Crab: [[-2,0], [0,1], [2,0]],
+                Monkey: [[-1,1], [1,1], [-1,-1], [1,-1]],
+                Crane: [[0,1], [-1,-1], [1,-1]],
+                Dragon: [[-2,1],[-1,-1],[1,-1],[2,1]],
+                Elephant: [[-1,1],[-1,0],[1,1],[1,0]],
+                Mantis: [[-1,1],[0,-1],[1,1]],
+                Boar: [[-1,0],[0,1],[1,0]],
+                Frog: [[-2,0], [-1,1], [1,-1]],
+                Goose: [[-1,1], [-1,0], [1,0], [1,-1]],
+                Horse: [[-1,0], [0,1], [0,-1]],
+                Eel: [[-1,1], [-1,-1], [1,0]],
+                Rabbit: [[-1,-1], [1,1], [2,0]],
+                Rooster: [[-1,-1], [-1,0], [1,0], [1,1]],
+                Ox: [[0,1], [0,-1], [1,0]],
+                Cobra: [[-1,0], [1,1], [1,-1]]
               },
-      deck: ['triangle', 'invTri', 'upDown', 'backDiag', 'forwDiag', 'cross', 'front'],
+      deck: ['Tiger', 'Crab', 'Monkey', 'Crane', 'Dragon', 'Elephant', 'Mantis', 'Boar', 'Frog', 'Goose', 'Horse', 'Eel', 'Rabbit', 'Rooster', 'Ox', 'Cobra'],
+      discard: [],
       selected: '',
       isSelected: false,
+      nextCard: [],
       player1Cards: ['',''],
       player2Cards: ['',''],
       validSquares: [],
       p1CardIndex: 0,
+      cardCss: ['card1 selected-card', 'card2']
     };
   }
 
   //From the props set the hands of players and update the deck.
   async componentWillMount() {
-    await this.setState({player1Cards: this.props.p1});
-    await this.setState({player2Cards: this.props.p2});
 
     let tempDeck = this.state.deck.slice();
+    let newDeckState = getCard.call(this, "player1Cards", tempDeck);
+    tempDeck = newDeckState[0];
+    await this.setState({player1Cards: [newDeckState[1][0], newDeckState[2][0]]})
+    newDeckState = getCard.call(this, "player2Cards", tempDeck);
+    await this.setState({player2Cards: [newDeckState[1][0], newDeckState[2][0]]})
+    await this.setState({deck: newDeckState[0]});
 
-    for(let i = 0; i<2; i++){
-      tempDeck.splice(tempDeck.indexOf(this.state.player1Cards[i]),1);
-      tempDeck.splice(tempDeck.indexOf(this.state.player2Cards[i]),1);
-    }
-    await this.setState({deck: tempDeck});
+    let nextCard = new Array(newDeckState[0][0]);
+    await this.setState({nextCard: nextCard});
   }
 
-  handleCardChoice(x){
-    let select = x;
+  selectThisCard(isCard1){
+    if(isCard1){
+      this.setState({cardCss: ["card1 selected-card", "card2"]})
+      this.setState({p1CardIndex: 0})
+    } else {
+      this.setState({cardCss: ["card1", "card2 selected-card"]})
+      this.setState({p1CardIndex: 1})
+    }
 
-    this.setState({p1CardIndex: select});
+    if(this.state.isSelected) {
+      this.setState({isSelected: false});
+      this.setState({selected: ''});
+    }
   }
 
   async handleClick(x,y) {
@@ -112,6 +115,7 @@ class Board extends Component {
       let tempSqr = getValidSquares(x,y,cardArr);
       let sqrL = tempSqr.length;
 
+      //Prevent capturing your own pieces
       for (let i = sqrL-1; i>=0; i--) {
         if (regexO.test(squares[tempSqr[i][0]][tempSqr[i][1]])) {
           tempSqr.splice(i,1);
@@ -136,6 +140,7 @@ class Board extends Component {
   renderSquare(x,y) {
     let classSqr = "square";
     
+    //If a piece is selected, only show valid move options, otherwise pieces are selectable.
     if(this.state.isSelected){
       for (let i = 0; i<this.state.validSquares.length; i++){
         if(this.state.validSquares[i][0] === x && this.state.validSquares[i][1] === y) {
@@ -147,7 +152,7 @@ class Board extends Component {
         classSqr = "square pointer";
       }
     }
-
+    //If piece selected, highlight accordingly
     if(this.state.selected[0] === x && this.state.selected[1] === y){
       classSqr = `square active pointer`;
     }
@@ -155,47 +160,58 @@ class Board extends Component {
     return <Square active={classSqr} value={this.state.squares[x][y]} onClick={() => this.handleClick(x,y)} />;  
   }
 
-  //TODO abstract away renderSquares to render only when changed
   render() {
     return (
       <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0,0)}
-          {this.renderSquare(0,1)}
-          {this.renderSquare(0,2)}
-          {this.renderSquare(0,3)}
-          {this.renderSquare(0,4)}
+        <div id="player-box">
+          <Card className="card1" card={this.state.player2Cards[0]}/>
+          <Card className="card2" card={this.state.player2Cards[1]}/>
         </div>
-        <div className="board-row">
-          {this.renderSquare(1,0)}
-          {this.renderSquare(1,1)}
-          {this.renderSquare(1,2)}
-          {this.renderSquare(1,3)}
-          {this.renderSquare(1,4)}
+        <div>
+          <div className="status">{status}</div>
+          <div className="board-row">
+            {this.renderSquare(0,0)}
+            {this.renderSquare(0,1)}
+            {this.renderSquare(0,2)}
+            {this.renderSquare(0,3)}
+            {this.renderSquare(0,4)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(1,0)}
+            {this.renderSquare(1,1)}
+            {this.renderSquare(1,2)}
+            {this.renderSquare(1,3)}
+            {this.renderSquare(1,4)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(2,0)}
+            {this.renderSquare(2,1)}
+            {this.renderSquare(2,2)}
+            {this.renderSquare(2,3)}
+            {this.renderSquare(2,4)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(3,0)}
+            {this.renderSquare(3,1)}
+            {this.renderSquare(3,2)}
+            {this.renderSquare(3,3)}
+            {this.renderSquare(3,4)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(4,0)}
+            {this.renderSquare(4,1)}
+            {this.renderSquare(4,2)}
+            {this.renderSquare(4,3)}
+            {this.renderSquare(4,4)}
+          </div>
         </div>
-        <div className="board-row">
-          {this.renderSquare(2,0)}
-          {this.renderSquare(2,1)}
-          {this.renderSquare(2,2)}
-          {this.renderSquare(2,3)}
-          {this.renderSquare(2,4)}
+        <div id="player-box">
+          <Card className={this.state.cardCss[0]} card={this.state.player1Cards[0]} onClick={() => this.selectThisCard(true)}/>
+          <Card className={this.state.cardCss[1]} card={this.state.player1Cards[1]} onClick={() => this.selectThisCard(false)} />
         </div>
-        <div className="board-row">
-          {this.renderSquare(3,0)}
-          {this.renderSquare(3,1)}
-          {this.renderSquare(3,2)}
-          {this.renderSquare(3,3)}
-          {this.renderSquare(3,4)}
+        <div>
+          <Card className="next-card" card={this.state.nextCard}/>
         </div>
-        <div className="board-row">
-          {this.renderSquare(4,0)}
-          {this.renderSquare(4,1)}
-          {this.renderSquare(4,2)}
-          {this.renderSquare(4,3)}
-          {this.renderSquare(4,4)}
-        </div>
-        <Cards nameOfCard1={this.state.player1Cards[0]} nameOfCard2={this.state.player1Cards[1]}/>
       </div>
     );
   }
@@ -207,28 +223,16 @@ class Game extends Component {
     this.state = {
       history: [],
       playerIsNext: true,
-      deck: ['triangle', 'invTri', 'upDown', 'backDiag', 'forwDiag', 'cross', 'front'],
-      p1Hand: ["",""],
-      p2Hand: ["",""],
       start: false
     }
   }
 
-  //deal cards to players and load board
-  async gameStart(){
-    let tempDeck = this.state.deck.slice();
-    let newDeckState = getCard.call(this, "p1Hand", tempDeck);
-    tempDeck = newDeckState[0];
-    await this.setState({p1Hand: [newDeckState[1][0], newDeckState[2][0]]})
-    newDeckState = getCard.call(this, "p2Hand", tempDeck);
-    await this.setState({p2Hand: [newDeckState[1][0], newDeckState[2][0]]})
-    await this.setState({deck: newDeckState[0]});
-
+  gameStart(){
     this.setState({start: true});
   }
 
   render() {
-    let gameState = this.state.start ? <Board p1={this.state.p1Hand} p2={this.state.p2Hand}/> : <Start onClick={() => this.gameStart()}/>;
+    let gameState = this.state.start ? <Board/> : <Start onClick={() => this.gameStart()}/>;
 
     return (
       <div className="game">
@@ -236,10 +240,6 @@ class Game extends Component {
 
           {gameState}
 
-        </div>
-        <div className="game-info">
-          <div>{/* status */}This is Onitama</div>
-          <ol>{/* TODO */}</ol>
         </div>
       </div>
     );
